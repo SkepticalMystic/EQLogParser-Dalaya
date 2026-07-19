b390976cc3b6d7e9a8ff0c72c90e365faf86d05a
SkepticalMystic <Dryvin@gmail.com>
2026-07-19 12:50:58 -0500

    Bump version to 1.6.0 (1.6.0)

--- EQLogParser/Properties/AssemblyInfo.cs
+++ EQLogParser/Properties/AssemblyInfo.cs
@@ -58,7 +58,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
 [assembly: XmlConfigurator(Watch = true)]
 [assembly: SupportedOSPlatform("windows10.0")]
--- EQLogParserInstall/EQLogParserInstall.iss
+++ EQLogParserInstall/EQLogParserInstall.iss
@@ -6,7 +6,7 @@
 #endif
 
 #define MyAppName "EQLogParser-Dalaya"
-#define MyAppVersion "1.5.0"
+#define MyAppVersion "1.6.0"
 #define MyAppPublisher "SkepticalMystic"
 #define MyAppURL "https://github.com/SkepticalMystic/EQLogParser-Dalaya"
 #define MyAppExeName "EQLogParser-Dalaya.exe"
